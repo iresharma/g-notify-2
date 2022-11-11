c2cd7fa2edade48e4f6841ae794437fd17de4ac1
Iresh Sharma <iresharma@Ireshs-MacBook-Pro.local>
2022-11-11 15:07:12 +0530

    :sparkles: navbar of default layout done

---
+++ components/navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="navbar">
+    <div class="left-container">
+      <Logo />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.navbar {
+  width: 100vw;
+  height: 7vh;
+  position: sticky;
+  top: 0;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  border-bottom: 0.5px solid grey;
+
+  padding: 0 1em;
+
+  .left-container {
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: false
+    }
+  }
+};
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <Navbar />
+      <el-main>
+        <slot />
+      </el-main>
+      <el-footer>
+        <slot name="Footer">
+            Footer
+        </slot>
+      </el-footer>
+    </el-container>
+  </div>
+</template>
